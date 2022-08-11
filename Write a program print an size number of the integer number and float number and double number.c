eeeda69277e034277204305fc8c2c93186f0a769
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-11 11:52:37 +0530

    Create Write a program print an size number of the integer number and float number and double number.c

---
+++ Write a program print an size number of the integer number and float number and double number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    //print an size number of the integer number and float number and double number
+    int number;
+    float f;
+    double d;
+    printf("The size of the number of integer %d\n",sizeof(number));
+    printf("The size of the number of double number %d\n",sizeof(d));
+    printf("The size of the number of float number %d\n",sizeof(f));
+
+
+
+
+
+
+
+
+    return 0;
+}
