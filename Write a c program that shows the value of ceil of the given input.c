5e65e0698f739d4792b51a2023d02f9a2cc10a12
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-15 21:53:41 +0530

    Create Write a c program that shows the value of ceil of the given input.c

---
+++ Write a c program that shows the value of ceil of the given input.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    float  a,b;
+
+    printf("Enter any Value :\n");
+    scanf("%f",&a);
+    float result = ceil(a);
+    printf("%f is %0.0f",a,result);
+
+
+
+    return 0;
+    }
