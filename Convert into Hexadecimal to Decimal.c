6738e062fde8baea83249ce2d87b361390005177
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-15 07:00:38 +0530

    Create Convert into Hexadecimal to Decimal.c

---
+++ Convert into Hexadecimal to Decimal.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    char a;
+    int b;
+    char upper,lower;
+
+printf("enter any hexadecimal case\n");
+scanf("%x",&b);
+//logic
+//using the library toupper
+upper=tolower(a);
+printf("the deciaml case %d",b);
+
+return 0;
+}
