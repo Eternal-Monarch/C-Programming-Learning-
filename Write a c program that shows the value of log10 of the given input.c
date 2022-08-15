17c11dfefff7d8f679cb0672bb37b585b3542480
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-15 21:41:06 +0530

    Create Write a c program that shows the value of log10 of the given input.c

---
+++ Write a c program that shows the value of log10 of the given input.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    float  a,b;
+
+    printf("Enter any Value :\n");
+    scanf("%f",&a);
+    float result=log10(a);
+    printf("%f is %f",a,result);
+
+
+
+    return 0;
+    }
