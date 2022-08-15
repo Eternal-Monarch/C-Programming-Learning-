e9a16ccd0abe072aa4574a7441dfeb2791490688
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-15 07:04:07 +0530

    Create convert into octal to Hexa-decimal.c

---
+++ convert into octal to Hexa-decimal.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    char a;
+    int b;
+    char upper,lower;
+
+printf("enter any Octal case\n");
+scanf("%o",&b);
+//logic
+//using the library toupper
+upper=tolower(a);
+printf("the Hexadeciaml case %x",b);
+
+return 0;
+}
