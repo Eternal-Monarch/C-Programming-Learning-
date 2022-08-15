b02f41c867167c060ced9f8ac046719c0655876d
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-15 18:24:51 +0530

    Create Square root.c

---
+++ Square root.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    double result;
+    printf("Enter any Value :\n");
+    scanf("%d",&a);
+    result=sqrt(a);
+    printf("%.0lf",result);
+
+
+
+    return 0;
+    }
