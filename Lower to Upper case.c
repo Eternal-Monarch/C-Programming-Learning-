eb34f5d7c3eb2e92cb6dc71c7d7f3f5b7c5dd0b1
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-15 06:22:52 +0530

    Create Lower to Upper case.c

---
+++ Lower to Upper case.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    char a;
+
+printf("enter any lowercase\n");
+scanf("%c",&a);
+//logic 
+//just subtraction 32 from the chracter
+printf("the uppercase is : %c",(a-32));
+
+return 0;
+}
