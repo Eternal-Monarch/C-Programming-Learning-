b1d2460f767f7f1ae08b6d1646584c10293929e3
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-15 16:35:38 +0530

    Create বৃত্তের ক্ষেত্রফল নির্ণয়ের সি প্রোগ্রাম.c

---
+++ বৃত্তের ক্ষেত্রফল নির্ণয়ের সি প্রোগ্রাম.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+
+    float radius;
+    float  area;
+    float PI=3.1416;
+    printf("enter radius \n");
+    scanf("%f",&radius);
+
+    float result=3.1416*radius*radius;
+
+
+    
+
+
+    printf("the result is %.4f",result);
+    return 0;
+    }
