a01748cd5e031e25dfda872b484e539b0cf9df9d
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-15 15:27:48 +0530

    Create Average of Two Numbers.c

---
+++ Average of Two Numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int number1,number2,sum,avg;
+printf("Enter Two Numbers : ");
+scanf("%d %d",&number1,&number2);
+sum=number1+number2;
+avg=sum/2;
+printf("The Sum IS : %d",avg);
+return 0;
+}
