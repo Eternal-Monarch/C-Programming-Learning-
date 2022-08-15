dc206483ad28e75ee44672f558a5fd7759806cf8
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-15 18:38:38 +0530

    Create x raised to the power of c program.c

---
+++ x raised to the power of c program.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int a,b;
+    int result;
+    printf("Enter any Value :\n");
+    scanf("%d %d",&a,&b);
+    result=pow(a,b);
+    printf("%d",result);
+
+
+
+    return 0;
+    }
