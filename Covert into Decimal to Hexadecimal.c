a04816343caa65c315017660d32a23bdce6a4553
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-15 06:59:17 +0530

    Create Covert into Decimal to Hexadecimal.c

---
+++ Covert into Decimal to Hexadecimal.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    char a;
+    int b;
+    char upper,lower;
+
+printf("enter any decimal case\n");
+scanf("%d",&b);
+//logic
+//using the library toupper
+upper=tolower(a);
+printf("the hexadeciaml case %x",b);
+
+return 0;
+}
