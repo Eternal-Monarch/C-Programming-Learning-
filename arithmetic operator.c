c9b0243f00f0b727e629c1118dbf7906d2ead7b0
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-15 15:25:38 +0530

    Create arithmetic operator.c

---
+++ arithmetic operator.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int number1,number2,sum;
+printf("Enter Two Numbers : ");
+scanf("%d %d",&number1,&number2);
+sum=number1+number2;
+printf("The Sum IS : %d",sum);
+return 0;
+}
