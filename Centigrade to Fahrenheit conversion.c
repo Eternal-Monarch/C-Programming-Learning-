bf2c2894014d163b9daf63b917dfb2f261bdd6fb
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-15 16:56:20 +0530

    Create Centigrade to Fahrenheit conversion.c

---
+++ Centigrade to Fahrenheit conversion.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+
+
+    float c, fahenheite;
+
+    printf("enter the centigrade : ");
+    scanf("%f",&c);
+    float result=(1.8*c)+32;
+    printf("the farn is : %.4f ",result);
+    return 0;
+    }
