6b39809933fd0bc1edf7da45cd2656697cf988e7
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-15 21:51:15 +0530

    Create Write a c program that shows the value of truncof the given input.c

---
+++ Write a c program that shows the value of truncof the given input.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    float  a,b;
+
+    printf("Enter any Value :\n");
+    scanf("%f",&a);
+    float result=trunc(a);
+    printf("%f is %0.1f",a,result);
+
+
+
+    return 0;
+    }
