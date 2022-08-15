8de1c0309b7edb230f262838e47b36b1ffdaaead
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-15 06:52:53 +0530

    Create Decimal to Octal.c

---
+++ Decimal to Octal.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    char a;
+    int b;
+    char upper,lower;
+
+printf("enter any decimal case\n");
+scanf("%d",&b);
+//logic
+//using the library toupper
+upper=tolower(a);
+printf("the octal sentence %o",b);
+
+return 0;
+}
