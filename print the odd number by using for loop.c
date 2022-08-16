6b4aed5adacc719ea8eecf1be3c49fb7ca39fbf8
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-17 01:59:24 +0530

    Create print the odd number by using for loop.c

---
+++ print the odd number by using for loop.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+
+    int a,b;
+    int temp;
+    int i=0;
+    for(i=1;i<=100;i++)
+    {
+        if(i%2!=0)
+            printf("%d \n",i);
+    }
+
+    return 0;
+
+}
