28faff96a60cc3dcd293eaeba1f7f242a4b4a502
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-16 18:24:28 +0530

    Create Vowel Determine By Using Or in C.c

---
+++ Vowel Determine By Using Or in C.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int
+main ()
+{
+
+    char ch;
+    scanf("%c",&ch);
+    if(ch=='a'|| ch=='e'||ch=='i'||ch=='o'||ch=='u')
+    printf("vowel\n");
+
+else
+{
+
+
+    printf("not vowel\n");
+    }
+
+
+
+    return 0;
+
+}
+
+
