8174d10ccf78cf7615733c6390510e970ce04f62
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-16 18:12:27 +0530

    Create দুটি সংখ্যার মধ্যে বৃহত্তম সংখ্যা নির্ণয়ের সি প্রোগ্রাম.c

---
+++ দুটি সংখ্যার মধ্যে বৃহত্তম সংখ্যা নির্ণয়ের সি প্রোগ্রাম.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int
+main ()
+{
+    int m,marks2;
+    printf("Enter The Marks : \n");
+    scanf("%d %d",&m,&marks2);
+    if(m>marks2)
+        printf("%d is greater than %d",m,marks2);
+
+ else if(m<marks2)
+        printf("%d is greater than %d",marks2,m);
+
+
+
+
+
+    return 0;
+
+}
+
+
