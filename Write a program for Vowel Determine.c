839f597c4ae187d8be1642f3a3f3b79bc9b18139
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-16 18:21:32 +0530

    Create Write a program for Vowel Determine.c

---
+++ Write a program for Vowel Determine.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int
+main ()
+{
+
+    char ch;
+    scanf("%c",&ch);
+    if(ch=='a')
+    printf("vowel\n");
+    else if(ch=='e')
+        {
+            printf("vowel\n");
+    }
+    else if(ch=='i')
+{
+
+
+printf("vowel\n");
+}
+else if(ch=='o')
+{
+
+ printf("vowel\n");
+}
+else if(ch=='u')
+{
+
+
+ printf("vowel\n");
+}
+else
+{
+
+
+    printf("not vowel\n");
+    }
+
+
+
+    return 0;
+
+}
+
+
