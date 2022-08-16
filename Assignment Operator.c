d7f3ea19f1c393688628d271d8a206ec9caa37a8
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-16 16:55:08 +0530

    Create Assignment Operator.c

---
+++ Assignment Operator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+
+    printf("Hello WOrld\n");
+    int x=5;
+    x+=5;//x=x+5
+    x-=5;//x=x-5
+    x*=5;//x=x*5
+    x/=5;//x=x/5
+    printf("%d\n",x);
+    return 0;
+}
