ba83ee7ba012a3b38a2fa8af14a770ca9e6e4062
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-17 02:05:18 +0530

    Create print the odd number by using do while loop.c

---
+++ print the odd number by using do while loop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+
+    int a,b;
+    int temp;
+    int i=0;
+    do
+    {
+        if(i%2!=0)
+            printf("%d \n",i);
+        i++;
+    }
+    while(i<=10);
+    return 0;
+
+}
