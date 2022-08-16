e491e872204ddb1196d57b045b72895224efeef8
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-16 17:32:47 +0530

    Create Relational Operator part1.c

---
+++ Relational Operator part1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int 
+main () 
+{
+  
+ 
+int a;
+  
+printf ("Enter a Number:\n");
+  
+scanf ("%d", &a);
+  
+if (a % 2 == 0)
+    
+printf ("Even\n");
+  
+if (a % 2 != 0)
+    
+ 
+printf ("Odd");
+  
+ 
+ 
+ 
+return 0;
+
+}
+
+
