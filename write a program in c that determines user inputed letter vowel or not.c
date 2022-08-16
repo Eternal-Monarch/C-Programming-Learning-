cd314ad5616d80f280e0747b46256124829586ea
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-16 23:25:40 +0530

    Create write a program in c that determines user inputed letter vowel or not.c

---
+++ write a program in c that determines user inputed letter vowel or not.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+
+    char character;
+    scanf("%c",&character);
+    switch(character)
+    {
+
+        case'a':
+        printf("vowel\n");
+        break;
+        case 'e':
+        printf("vowel\n");
+        break;
+        case 'i':
+        printf("vowel\n");
+        break;
+        case 'o':
+        printf("vowel\n");
+        break;
+        case 'u':
+        printf("vowel\n");
+        break;
+        default:
+        printf("not vowel\n");
+
+    }
+return 0;
+}
