0726638ddc11f9162d3fe0c1ea40c8d3ba3eb3ec
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-16 17:19:52 +0530

    Create Unary Operator.c

---
+++ Unary Operator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+
+
+    int x = 10;
+    int result = x++;
+    //int result=-x;
+    //for showing positive to negative result of a positive integer value
+    printf("x is : = %d\n",x);
+    printf("y is : = %d\n",result);
+
+    return 0;
+}
