d4258878c24ff74acafb6d8f5e4eb9a18eefe365
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-19 00:59:23 +0530

    Create factorial of a number by using do while loop.c

---
+++ factorial of a number by using do while loop.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    int num1;
+    int temp=1;
+    int fact=1;
+
+    printf("enter a number : ");
+    scanf("%d",&num1);
+    do
+
+       {
+
+        fact=fact*i;
+        i++;
+       }
+       while(i<=num1);
+       printf("%d factorial is %d\n",num1,fact);
+//fact=temp;
+
+
+
+
+
+
+    return 0;
+
+
+}
