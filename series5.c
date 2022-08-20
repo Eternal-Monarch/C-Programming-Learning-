846fc0b506d4d4c9794bec611d5e71ee413843ed
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-21 01:43:20 +0530

    Create series5.c

---
+++ series5.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+
+
+{
+    int num,num2,i,j,a=1,sum=0,count=0,b=2;
+    printf("enter an number : \n");
+    scanf("%d",&num);
+    for(i=1; i<=num;i=i+3)
+    {
+
+            printf("%d + ",i);
+            count=count+i;
+        }
+
+
+    printf("= %d",count);
+
+
+
+    return 0;
+}
