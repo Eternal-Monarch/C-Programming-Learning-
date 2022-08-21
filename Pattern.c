98f4e1d0cc5415f681b21d3ef0c12f0ee14ef467
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-21 19:25:55 +0530

    Create Pattern.c

---
+++ Pattern.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    int i;
+    int j;
+    printf("enter the number : ");
+    scanf("%d",&num);
+    printf("1 \n");
+    for(i=1;i<=2;i++)
+    
+    {
+      printf("%d ",i);
+     
+        
+    }  
+    printf("\n");
+for(j=1;j<=num;j++)
+    printf("%d ",j);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+
+
+}
