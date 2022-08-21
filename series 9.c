189595ccbd7dc275b2abac7e80e4e3a493b5e5b2
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-21 17:22:15 +0530

    Create series 9.c

---
+++ series 9.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main ()
+{
+
+
+    int i,j;
+    int count=1
+    ;
+    int num;
+    printf("Enter a number : ");
+    scanf("%d",&num);//4
+    for(i=1;i<=num;i++)//1 2 3 4
+    {
+        if(i%2!=0)//2 4
+{
+    printf("%d * ",i); //2 4
+        count=count*i;//0
+        //0=0*i
+    }
+    }
+    printf(" = %d" ,count);
+    return 0;
+}
