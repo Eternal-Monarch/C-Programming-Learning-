c3244276a44eec7ec796ab0d7bf11e076dc7379b
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-22 00:10:41 +0530

    Create 14.pattern.c

---
+++ 14.pattern.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int num,row,col;
+    int i;
+    int j;
+    printf("enter the number row : ");
+    scanf("%d",&num);//3
+   for(j=num;j>=1;j--)//row
+   {
+       for(i=1;i<=j;i++)//col
+        {
+            printf("%d ",j%2);
+        }
+        printf("\n");
+
+   }
+    return 0;
+
+
+}
