b8e540add4c248e7a135d94146c14710250099ef
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-26 15:09:43 +0530

    Create pattern7.c

---
+++ pattern7.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+int i,j;
+int num;
+printf("enter a number : ");
+scanf("%d",&num);
+for(i=0;i<=num;i++)//row
+{
+
+    for(j=1;j<=i;j++)
+    {
+
+        printf("%c ",i+64);
+    }
+
+
+    printf("\n");
+
+}
+for(j=num-1;j>=1;j--)
+
+
+{
+
+     for(i=1;i<=j;i++)
+    {
+
+        printf("%c ",j+64);
+    }
+    printf("\n");
+
+}
+return 0;
+
+}
+
+
+
