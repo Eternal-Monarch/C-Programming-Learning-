7253be00548b6ba38695cc298d867ebab72d4235
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-10-07 14:41:50 +0530

    Create Create 2D array.c

---
+++ Create 2D array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int
+main ()
+{
+  int i, j;
+  int array[3][4] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+  for (i = 0; i < 3; i++)
+    {
+      for (j = 0; j < 3; j++)
+	{
+	  printf ("%d\t",array[i][j]);
+
+	}
+      printf ("\n");
+    }
+      return 0;
+
+    }
+
